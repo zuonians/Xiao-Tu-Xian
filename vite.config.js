fa88dd34f49da2636593da5ecdaef379f48ddf3f
zuonians <zts63590@gmail.com>
2025-07-25 20:36:15 +0800

    element主题色的定制

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,10 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        // 1. 配置elementPlus采用sass样式配色系统
+        ElementPlusResolver({ importStyle: "sass" }),
+      ],
     }),
   ],
   resolve: {
@@ -28,4 +31,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 2. 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+          `,
+      }
+    }
+  }
 })
