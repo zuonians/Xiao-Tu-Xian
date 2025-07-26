89604c840a4f6e5a8a3010bd17fc8b739e3eb751
zuonians <zts63590@gmail.com>
2025-07-26 11:29:01 +0800

    项目整体路由设计

--- eslint.config.js
+++ eslint.config.js
@@ -21,4 +21,10 @@ export default defineConfig([
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
+
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
 ])
