32e4b7aea3a7c367dd785c70d777c198e38304f3
zuonians <zts63590@gmail.com>
2025-07-27 10:22:21 +0800

    category-轮播图实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -1,7 +1,7 @@
 import request from '@/utils/http';
 
 
-export const getCategoryAPI = (id) => {
+export function getCategoryAPI (id) {
   return request({
     url:'/category',
     params:{
--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,18 +1,35 @@
 <script setup>
 import { getCategoryAPI } from '@/apis/category';
+import { getBannerAPI } from '@/apis/home';
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
 
 // 获取数据
-const categoryData = ref()
+const categoryData = ref({})
 const route = useRoute()
 const getCategory = async () => {
   const res = await getCategoryAPI(route.params.id)
   categoryData.value = res.result
-  console.log(categoryData) // 打印获取到的分类数据
 }
+
 onMounted(() => {
   getCategory()
+})
+
+// 获取banner
+
+const bannerList = ref([]);
+
+const getBanner = async () => {
+  const res = await getBannerAPI({
+    distributionSite: '2'
+  });
+  console.log(res);
+  bannerList.value = res.result
+}
+
+onMounted(() => {
+  getBanner();
 })
 </script>
 
@@ -25,6 +42,14 @@ onMounted(() => {
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
           <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
+      </div>
+      <!-- 轮播图 -->
+      <div class="home-banner">
+        <el-carousel height="500px">
+          <el-carousel-item v-for="item in bannerList" :key="item.id">
+            <img :src="item.imgUrl" alt="">
+          </el-carousel-item>
+        </el-carousel>
       </div>
     </div>
   </div>
@@ -107,6 +132,17 @@ onMounted(() => {
 
   .bread-container {
     padding: 25px 0;
+  }
+}
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  margin: 0 auto;
+  z-index: 98;
+
+  img {
+    width: 100%;
+    height: 500px;
   }
 }
 </style>
